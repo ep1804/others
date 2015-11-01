9bcbf107e5a6a72f19fb84c49746c939e25bbfd6
Soonmok Kwon <ep1804@gmail.com>
2015-11-01 22:18:17 +0900

    drill

--- drill/src/trie/IStringST.java
+++ drill/src/trie/IStringST.java
@@ -3,6 +3,9 @@ package trie;
 public interface IStringST<V> {
 	public void put(String key, V val);
 	public V get(String key);
-	public boolean contains(String key);	
-
+	public boolean contains(String key);
+	
+	public Iterable<String> keys();
+	public Iterable<String> keysWithPrefix(String prefix);
+	public String longestPrefix(String s);
 }
