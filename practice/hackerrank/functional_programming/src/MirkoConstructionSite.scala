05f3f252b4119dc99a43438b9211126067dd97eb
Soonmok Kwon <ep1804@gmail.com>
2015-12-28 08:46:54 +0900

    practice

---
+++ practice/hackerrank/functional_programming/src/MirkoConstructionSite.scala
@@ -0,0 +1,10 @@
+
+
+object MirkoConstructionSite {
+  def main(args: Array[String]): Unit = {
+    val in = io.Source.stdin.getLines
+    
+    val nq = in.next.split(" ").map(_.toInt).toList
+    
+  }
+}
