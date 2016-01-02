7e3a4ef61f833452db29318b62f28970c64237da
Soonmok Kwon <ep1804@gmail.com>
2016-01-02 23:33:09 +0900

    fibonacci scala stream

--- practice/hackerrank/functional_programming/src/euler/2.scala
+++ practice/hackerrank/functional_programming/src/euler/2.scala
@@ -1,9 +1,19 @@
 package euler
 
 object EvenFibonacci {
-  val in = io.Source.stdin.getLines
-  val T = in.next.toInt
-  val ns = in.take(T).map(_.toLong)
-  
-  println(Long.MaxValue)
+
+  // Ref: http://www.scala-lang.org/api/current/index.html#scala.collection.immutable.Stream
+  lazy val fibs: Stream[Long] = 1 #:: 2 #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+
+  def sumFib(n: Long): BigInt =
+    fibs.takeWhile(_ <= n).filter(_ % 2 == 0).foldLeft(BigInt(0))((a, b) => a + BigInt(b))
+  // Note: you cannot NOT change the takeWhile to filter.
+
+  def main(args: Array[String]): Unit = {
+    val in = io.Source.stdin.getLines
+    val T = in.next.toInt
+    val ns = in.take(T).map(_.toLong)
+
+    ns foreach { n => println(sumFib(n).toString()) }
+  }
 }
