304bfe4225442ee8c5593b28d2163dd04e1ba0c5
Soonmok Kwon <ep1804@gmail.com>
2016-01-05 01:26:13 +0900

    prime factors

--- practice/hackerrank/functional_programming/src/euler/3.scala
+++ practice/hackerrank/functional_programming/src/euler/3.scala
@@ -2,23 +2,19 @@ package euler
 
 object LargestPrimeFactor {
   
-  lazy val from3: Stream[Long] = 3 #:: from3.map(_ + 1)
+  lazy val from3: Stream[Long] = 
+    3L #:: from3.map(_ + 1)
   
-  lazy val primes: Stream[Int] = 2 #:: from3.filter(i => primes.takeWhile(j => j * j <= i).forall(k => i % k > 0)).map(_.toInt)
-  
-//  def maxFactor(n: Long): Long = {
-//    lazy val ns: Stream[Long] = (n) #:: ns.map(x => x - 1)
-//    ns.takeWhile(n % _ == 0).takeWhile(isPrime)(0)
-//  }
-  
+  lazy val primes: Stream[Long] = 
+    2L #:: from3.filter(i => primes.takeWhile(j => j * j <= i).forall(k => i % k > 0))
+    
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
     
     val T = in.next.toInt
     val ns = in.take(T).map(_.toLong)
     
-    ns foreach ( x => {
-      
+    ns foreach ( x => {      
       val primeFactors = primes.takeWhile(_ <= x).filter(p => x % p == 0)
       println(primeFactors.last)
     })
