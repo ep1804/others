5373909a021336268a6a48ccdab889e88c87cda5
Soonmok Kwon <ep1804@gmail.com>
2016-01-06 00:30:07 +0900

    hybrid

--- practice/hackerrank/functional_programming/src/euler/3.scala
+++ practice/hackerrank/functional_programming/src/euler/3.scala
@@ -1,21 +1,36 @@
 package euler
 
 object LargestPrimeFactor {
+
+  lazy val from3by2: Stream[Long] = 3L #:: from3by2.map(_ + 2)
   
-  def largestPrimeFactor(b : BigInt) = {
-  def loop(f:BigInt, n: BigInt): BigInt =
-     if (f == n) n else 
-     if (n % f == 0) loop(f, n / f) 
-     else loop(f + 1, n)
-  loop (BigInt(2), b)
-}
+  lazy val primes: Stream[Long] = 
+    2L #:: from3by2.filter(i => primes.takeWhile(j => j * j <= i).forall(i % _ > 0))
+
+  // c.f.
+  //def isPrime(n: Long): Boolean = primes.dropWhile(n % _ != 0)(0) == n
+
+  // repeat division by p until there's no p factor in n
+  def divRep(n: Long, p: Long): Long = 
+    if(n % p != 0) n
+    else divRep(n / p, p)
     
+  def solve(n: Long): Long = {
+    val p = primes.dropWhile(n % _ != 0)(0) // smallest prime factor
+    if( p == n ) n
+    else {
+      val n2 = divRep(n, p)
+      if(n2 == 1) p
+      else solve(n2)
+    }    
+  }
+
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
-    
+
     val T = in.next.toInt
-    val ns = in.take(T).map(BigInt(_))
+    val ns = in.take(T).map(_.toLong) 
     
-    ns map largestPrimeFactor foreach println
-  }  
+    ns map solve foreach println
+  }
 }
