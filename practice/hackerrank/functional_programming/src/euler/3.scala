83410483be0c61889829b969320576a53703fcf8
Soonmok Kwon <ep1804@gmail.com>
2016-01-05 23:14:47 +0900

    from3by2

--- practice/hackerrank/functional_programming/src/euler/3.scala
+++ practice/hackerrank/functional_programming/src/euler/3.scala
@@ -2,11 +2,10 @@ package euler
 
 object LargestPrimeFactor {
   
-  lazy val from2: Stream[Long] = 2L #:: from2.map(_ + 1)
+  lazy val from3by2: Stream[Long] = 3L #:: from3by2.map(_ + 2)
   
-  def sieve(s: Stream[Long]): Stream[Long] = s.head #:: sieve(s.tail.filter( _ % s.head != 0))
-  
-  val primes = sieve(from2)
+  lazy val primes: Stream[Long] = 
+    2L #:: from3by2.filter(i => primes.takeWhile(j => j * j <= i).forall(k => i % k > 0))
     
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
@@ -14,8 +13,8 @@ object LargestPrimeFactor {
     val T = in.next.toInt
     val ns = in.take(T).map(_.toLong)
     
-    ns foreach ( x => {      
-      val primeFactors = primes.takeWhile(_ <= x).filter(p => x % p == 0)
+    ns foreach ( x => {
+      val primeFactors = primes.takeWhile(_ <= x).filter(x % _ == 0)
       println(primeFactors.last)
     })
   }  
