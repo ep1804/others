1b58540b92d459701471f7dc7e5c14149623b44d
Soonmok Kwon <ep1804@gmail.com>
2016-01-07 00:01:17 +0900

    gcd lcm

--- practice/hackerrank/functional_programming/src/euler/5.scala
+++ practice/hackerrank/functional_programming/src/euler/5.scala
@@ -1,6 +1,15 @@
 package euler
 
 object SmallestMultiple {
+
+  def gcd(a: Int, b: Int): Int = 
+    if(b == 0) a 
+    else if(a > b) gcd(b, a % b)
+    else gcd(a, b % a)
+  
+  def lcm(a: Int, b: Int): Int = a * b / gcd(a, b)
+
+  def solve(n: Int): Int = (1 to n).toList.foldLeft(1)(lcm)
   
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
@@ -8,6 +17,6 @@ object SmallestMultiple {
     val T = in.next.toInt
     val ns = in.take(T).map(_.toInt)
     
-    ns foreach println
+    ns map(solve(_)) foreach println
   }
 }
