47609641d397f80ed56ce1d885d882c70768d3ea
Soonmok Kwon <ep1804@gmail.com>
2016-01-17 15:30:52 +0900

    lattice dp

--- practice/hackerrank/functional_programming/src/euler/15.scala
+++ practice/hackerrank/functional_programming/src/euler/15.scala
@@ -1,19 +1,29 @@
 package euler
 
+import scala.collection.mutable.Map
+
 object LatticePaths {
   
   val modulo = 1000000007L
   
-  def lattice(n: Int, m: Int): Long = {
+  val latticeBuf = Map[(Int, Int), BigInt]()
+  
+  // n <= m
+  def lattice(n: Int, m: Int): BigInt = {
     if(n == 1) m + 1
-    else if(m == 1) n + 1
-    else lattice(n - 1, m) + lattice(n, m - 1)
+    else if(latticeBuf.contains((n, m)))
+        latticeBuf((n, m))
+    else {
+      val res = if (n == m) 2 * lattice(n - 1, m) else lattice(n - 1, m) + lattice(n, m - 1)
+      latticeBuf.put((n, m), res)
+      res
+    }
   }
   
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
-    val t = in.next.toInt
-    val nms = in.take(t).map(_.trim.split("\\s+").map(_.toInt))
+    val t = in.next.trim.toInt
+    val nms = in.take(t).map(_.trim.split("\\s+").map(_.toInt).sorted)
     
     nms foreach {case Array(n, m) => println(lattice(n, m) % modulo)}
   }
