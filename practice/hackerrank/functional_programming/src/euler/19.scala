80ec03edcf31e5089a4f1a67c2bc47d45f0c545c
Soonmok Kwon <ep1804@gmail.com>
2016-01-23 03:45:30 +0900

    sundays

--- practice/hackerrank/functional_programming/src/euler/19.scala
+++ practice/hackerrank/functional_programming/src/euler/19.scala
@@ -2,16 +2,34 @@ package euler
 
 object CountingSundays {
   
-  val mLen = Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-  
+  val mLen     = Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
   val mLenLeap = Array(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
   
-  def leap(y: Long): Boolean = if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) true else false
+  def leap(y: Long): Boolean = 
+    if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) true else false
   
-  // Days in Month from Jan 1 1900
-  val year1400 = (1900 to 3300).flatMap(y => if(leap(y)) mLenLeap else mLen)
+  // From each month from 1900, compute days in month
+  val m = (1900 to (2900 + 400 * 7)).flatMap(y => if(leap(y)) mLenLeap else mLen)
   
-  // Days in month from 
+  // For each 1st days of every months from 1900, 
+  // compute number of days from Dec. 31, 1899 (Sunday)
+  lazy val mCum: Stream[Long] = 1 #:: (mCum zip m).map{ case (a, b) => a + b }
+  
+  // For each 1st days of every months from 1900, 
+  // compute date (Sunday = 0, Monday = 1, and so on)
+  val mBuf = mCum.take((1001 + 400 * 7) * 12).map(_ % 7).toVector
+  
+  def sundays(d: Tuple2[Tuple3[Long, Long, Long], Tuple3[Long, Long, Long]]): Int = d match {
+    case ((yo1, m1, d1), (yo2, m2, d2)) => {
+      val y1 = 1900 + (yo1 - 1900) % (400 * 7)
+      val y2 = y1 + (yo2 - yo1)
+      
+      val idx1 = ((y1 - 1900) * 12 + (m1 - 1) + (if(d1 > 1) 1 else 0)).toInt
+      val idx2 = ((y2 - 1900) * 12 + (m2 - 1) + 1).toInt
+      
+      mBuf.drop(idx1).take(idx2 - idx1).count(_ == 0)
+    }
+  }
   
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
@@ -19,7 +37,7 @@ object CountingSundays {
     val ds = in.take(2 * t).
       map { s => val ns = s.split("\\s+").map(_.toLong); (ns(0), ns(1), ns(2)) }.
       grouped(2).map { case Seq(a, b) => (a, b) }
-
-    ds foreach println
+    
+    ds foreach (d => println(sundays(d)))
   }
 }
