3a8eab1c9058d5d7f7c0d83089510c955a40ed96
Soonmok Kwon <ep1804@gmail.com>
2016-01-26 03:16:09 +0900

    practice

--- practice/hackerrank/functional_programming/src/euler/E025.scala
+++ practice/hackerrank/functional_programming/src/euler/E025.scala
@@ -1,25 +1,22 @@
 package euler
 
-object E025 {
+object NDigitFibonacciNumber {
 
   lazy val fib: Stream[BigInt] =
     1 #:: 1 #:: (fib zip fib.tail).map { case (a, b) => (a + b) }
 
-  // fib digits and index
-  lazy val fib2 =
-    (fib zip Stream.from(1)).map { case (a, b) => (a.toString.length, b) }
+  // (digits, index)
+  lazy val fib2 = (0, 0) #:: (fib zip Stream.from(1)).map { case (a, b) => (a.toString.length, b) }
 
-  lazy val fib3: Stream[(Int, Int)] =
-    fib2(0) #:: (fib2 zip fib2.tail).filter { case ((a, _), (b, _)) => a != b }.map { case (a, b) => b }
+  // series of fibonacci indices where digit changes)
+  lazy val fib3: Stream[Int] = (fib2 zip fib2.tail).filter { case ((a, _), (b, _)) => a != b }. map { case (a, b) => b._2 }
 
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
     val t = in.next.toInt
     val ns = in.take(t).map(_.toInt)
 
-    ns foreach { n => println(fib3(n)) }
-    
-    println(fib3.take(5000).mkString(" "))
+    ns foreach { n => println(fib3(n - 1)) }
 
     /*
     println(fib.take(50).mkString(" "))
