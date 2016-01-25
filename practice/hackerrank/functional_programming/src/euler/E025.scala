090d14c8e41226b0b63b164ec7c5a95b32a28713
Soonmok Kwon <ep1804@gmail.com>
2016-01-26 03:20:05 +0900

    n digit fibonacci cache

--- practice/hackerrank/functional_programming/src/euler/E025.scala
+++ practice/hackerrank/functional_programming/src/euler/E025.scala
@@ -6,17 +6,22 @@ object NDigitFibonacciNumber {
     1 #:: 1 #:: (fib zip fib.tail).map { case (a, b) => (a + b) }
 
   // (digits, index)
-  lazy val fib2 = (0, 0) #:: (fib zip Stream.from(1)).map { case (a, b) => (a.toString.length, b) }
+  lazy val fib2 = 
+    (0, 0) #:: (fib zip Stream.from(1)).map { case (a, b) => (a.toString.length, b) }
 
   // series of fibonacci indices where digit changes)
-  lazy val fib3: Stream[Int] = (fib2 zip fib2.tail).filter { case ((a, _), (b, _)) => a != b }. map { case (a, b) => b._2 }
+  //lazy val fib3: Stream[Int] = 
+  //  (fib2 zip fib2.tail).filter { case ((a, _), (b, _)) => a != b }. map { case (a, b) => b._2 }
 
+  val fib2Cache = fib2.takeWhile{ case(a, b) => a <= 5000}.toArray
+  val fib3Cache = (fib2 zip fib2.tail).filter { case ((a, _), (b, _)) => a != b }. map { case (a, b) => b._2 }
+    
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
     val t = in.next.toInt
     val ns = in.take(t).map(_.toInt)
 
-    ns foreach { n => println(fib3(n - 1)) }
+    ns foreach { n => println(fib3Cache(n - 1)) }
 
     /*
     println(fib.take(50).mkString(" "))
