048ed9f5b5080ef0ccf37a54c58efc50c989a507
Soonmok Kwon <ep1804@gmail.com>
2016-01-27 00:56:08 +0900

    comment for golden ratio method

--- practice/hackerrank/functional_programming/src/euler/E025.scala
+++ practice/hackerrank/functional_programming/src/euler/E025.scala
@@ -2,17 +2,22 @@ package euler
 
 object NDigitFibonacciNumber {
 
-  // Solution by using closed-form expression of fibonacci sequence
-  // https://en.wikipedia.org/wiki/Golden_ratio#Relationship_to_Fibonacci_sequence
   
   val ¥õ = (1.0 + math.sqrt(5.0)) / 2.0  // golden ratio
   
-  // fibonacci number digits
-  // Approximation not using (-¥õ)^n term. But no problem until digit 5000
-  def fd(n: Int) = (n * math.log10(¥õ) - math.log10(5) / 2 + 1).toInt 
-  
+  // Fibonacci number's length is computed based on closed-form expression of Fibonacci sequence.
+  // https://en.wikipedia.org/wiki/Golden_ratio#Relationship_to_Fibonacci_sequence
+  // Strictly following the formula, implementation should be as follows:
+  // def fd(n: Int) = math.log10((math.pow(¥õ, n) - math.pow(-¥õ, -n))/math.sqrt(5)).toInt + 1
+  // But it is very slow when n is large. So, 
+  // Approximation is used not using (-¥õ)^n term. 
+  // This is ok when n is 2 ~ 5000
+  def fd(n: Int) = (n * math.log10(¥õ) - math.log10(5) / 2).toInt + 1
+
   // (digits, index)
+  // (0, 0) is inserted at the first position for convenience of building fdi2 later
   lazy val fdi = (0, 0) #:: (1, 1) #:: Stream.from(2).map(x => (fd(x), x))
+  
   
   // series of fibonacci indices where digit changes
   lazy val fdi2: Stream[Int] = 
@@ -25,6 +30,8 @@ object NDigitFibonacciNumber {
     val t = in.next.toInt
     val ns = in.take(t).map(_.toInt)
    
+    println((1 to 20).map(fd(_)).mkString(" "))
+    
     ns foreach { n => println(fdi3(n-1)) }
   }
 }
