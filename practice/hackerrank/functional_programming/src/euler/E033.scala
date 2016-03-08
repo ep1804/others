70b91493c9ba84d039b1fb1618494e7906623ee0
Soonmok Kwon <ep1804@gmail.com>
2016-03-09 00:31:12 +0900

    this was hard...

--- practice/hackerrank/functional_programming/src/euler/E033.scala
+++ practice/hackerrank/functional_programming/src/euler/E033.scala
@@ -1,111 +1,95 @@
 package euler
 
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable.Set
-
 object DigitCancelingFractions {
 
-  def cancelables(lim: Map[Char, Int], k: Int): Set[Map[Char, Int]] = {
+  var cache1: IndexedSeq[String] = null // for Int to String conversion
+  var cache2: Map[String, Int] = null // for String to Int conversion
 
-    val buf = Set[Map[Char, Int]]()
-
-    def loop(lim: Map[Char, Int], k: Int, acc: Map[Char, Int]): Unit = {
-      if (k == 0) {
-        buf += acc
-        return
-      }
-      for ((ch, n) <- lim) {
-        if (n > 1) {
-          if (acc.contains(ch)) {
-            loop((lim - ch) + (ch -> (n - 1)), k - 1, (acc - ch) + (ch -> (acc(ch) + 1)))
-          } else {
-            loop((lim - ch) + (ch -> (n - 1)), k - 1, acc + (ch -> 1))
-          }
-        } else {
-          if (acc.contains(ch)) {
-            loop((lim - ch), k - 1, (acc - ch) + (ch -> (acc(ch) + 1)))
-          } else {
-            loop((lim - ch), k - 1, acc + (ch -> 1))
-          }
-        }
-      }
-    }
-
-    loop(lim, k, Map[Char, Int]())
-    buf
+  implicit class IntExtension(x: Int) {
+    def toStringFast: String = cache1(x)
   }
 
-  def solve(digit: Int, k: Int): Seq[Int] = {
+  implicit class StringExtension(x: String) {
 
-    val nuBuf = Set[Int]()
-    val deBuf = Set[Int]()
+    def toIntFast: Int = cache2(x.dropWhile(_ == '0'))
 
-    def check(a: Int, b: Int): Boolean = {
-
-      val a1 = a.toString
-      val b1 = b.toString
-
-      val a2 = a1.zipWithIndex
-      val b2 = b1.zipWithIndex
-
-      val a3 = a2.filter(x => x._1 != '0' && b1.contains(x._1))
-      if (a3.length < k) return false
-
-      val b3 = b2.filter(x => x._1 != '0' && a1.contains(x._1))
-      if (b3.length < k) return false
-
-      val a4 = a3.groupBy(_._1).map { case (x, y) => (x, y.map(_._2)) }
-      val b4 = b3.groupBy(_._1).map { case (x, y) => (x, y.map(_._2)) }
-
-      val lim = a4.keys.map(k => (k, math.min(a4(k).size, b4(k).size))).toMap
-      if (lim.map(_._2).sum < k) return false
-
-      val cancelables1 = cancelables(lim, k)
-
-      // cancelation cases. 
-      // each tuple denotes: (indices of canceled digits in number a, indices of canceled digits in number b) 
-      val cases = (for { cb <- cancelables1 } yield cb.map {
-        case (k, v) => (k, for {
-          a <- a4(k).combinations(v)
-          b <- b4(k).combinations(v)
-        } yield (a, b))
-      }.values.foldLeft(Iterator((IndexedSeq[Int](), IndexedSeq[Int]()))) {
-        case (xs, ys) => for {
-          (xa, xb) <- xs
-          (ya, yb) <- ys
-        } yield (xa ++ ya, xb ++ yb)
-      }).flatten
-
-      // canceled numbers
-      val cs = cases.map { cs =>
-        (a2.filter(x => !cs._1.contains(x._2)).map(_._1).mkString("").toInt,
-          b2.filter(x => !cs._2.contains(x._2)).map(_._1).mkString("").toInt)
+    // different from filter in that one character in s is used to delete only one character in x
+    def remove(s: String): String = {
+      def loop(rm: String, acc: String): String = {
+        if (rm.size == 0) return acc
+        else {
+          val c = rm(0)
+          val s1 = acc.takeWhile(_ != c)
+          loop(rm.drop(1), s1 + acc.drop(s1.size + 1))
+        }
       }
-
-      !cs.forall { case (ac, bc) => a.toDouble / b != ac.toDouble / bc }
+      loop(s, x)
     }
-    
-//    println(check(4808, 8414))
-//    println(check(3016, 6032))
+
+    // check if order of characters in s is same in x
+    def checkOrder(s: String): Boolean = {
+      var i = 0
+      for (c <- s) {
+        i = x.indexOf(c, i) + 1
+        if (i == 0) return false
+      }
+      true
+    }
+
+    // different from built-in 'combinations' in that this preserves source order
+    def combinationsInOrder(n: Int): Iterator[String] = {
+      x.zipWithIndex.combinations(n).map(xs => xs.map(_._1).mkString)
+    }
+
+    def repeat(n: Int): String = (1 to n).toList.map(_ => "0").mkString
+
+  }
+
+  // n1 / d1 == n2 / d2
+  def solve(digit: Int, k: Int): Seq[Int] = {
 
     val start = math.pow(10, digit - 1).toInt
     val end = math.pow(10, digit).toInt - 1
 
-    val originals = for {
-      a <- start to end
-      b <- (a + 1) to end
-      if check(a, b)
-    } yield (a, b)
-    
-    originals foreach println
+    val d2end = math.pow(10, digit - k).toInt - 1
 
-    val nd = originals.unzip
+    val fractions = for {
+      n1 <- start to (end - 1)
+      val n1str = n1.toStringFast
+
+      n2str <- n1str.combinationsInOrder(digit - k) if !n2str.forall(_ == '0')
+
+      val nCancel = n1str.remove(n2str)
+      if !nCancel.contains('0')
+
+      val n2 = n2str.toIntFast
+
+      d2 <- (n2 + 1) to math.min(end * n2 / n1, d2end) 
+      if (n1 * d2) % n2 == 0
+
+      val d1 = n1 * d2 / n2
+
+      val d1str = d1.toStringFast
+      val d2str = d2.toStringFast
+      val d2strZeroPadded = "0".repeat(digit - k - d2str.length) + d2str // consider case 1083/1805 = 3/5
+
+      if (nCancel + d2strZeroPadded).sorted == d1str.sorted
+
+      if d1str.checkOrder(d2strZeroPadded) // consider case 1040/1508 = 40/58
+    } yield (n1, d1)
+
+    //fractions.distinct.sortBy(_._1) foreach println
+
+    val nd = fractions.distinct.unzip
     List(nd._1.sum, nd._2.sum)
   }
 
   def main(args: Array[String]): Unit = {
     val in = io.Source.stdin.getLines
     val Array(n, k) = in.next.trim.split(" ").map(_.toInt)
+
+    cache1 = (0 to math.pow(10, n).toInt - 1).map(_.toString)
+    cache2 = cache1.zipWithIndex.toMap
 
     println(solve(n, k).mkString(" "))
   }
