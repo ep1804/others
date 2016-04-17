1f5e2edb35373017a805299b443efa008d00b8e2
Soonmok Kwon <ep1804@gmail.com>
2016-04-18 03:09:08 +0900

    mergesort elementary

--- drill/src/sort/MergeSort.java
+++ drill/src/sort/MergeSort.java
@@ -7,14 +7,40 @@ import java.util.Scanner;
 @SuppressWarnings("rawtypes")
 public class MergeSort {
 
+	private static Comparable[] buf;
+
 	public static void sort(Comparable[] a) {
+		buf = new Comparable[a.length];
+		for (int i = 0; i < a.length; i++)
+			buf[i] = a[i];
+		sort(a, buf, 0, a.length);
 	}
 
 	public static void sort(Comparable[] a, Comparable[] buf, int lo, int hi) {
-		
+		if (hi - lo < 2)
+			return;
+		int mid = (lo + hi) / 2;
+		sort(buf, a, lo, mid);
+		sort(buf, a, mid, hi);
+		merge(buf, a, lo, mid, hi);
 	}
 
 	private static void merge(Comparable[] from, Comparable[] to, int lo, int mid, int hi) {
+		int i = lo;
+		int j = mid;
+
+		for (int k = lo; k < hi; k++) {
+			if (i == mid) {
+				to[k] = from[j++];
+			} else if (j == hi) {
+				to[k] = from[i++];
+			} else if (from[i].compareTo(from[j]) < 0) {
+				to[k] = from[i++];
+			} else {
+				to[k] = from[j++];
+			}
+		}
+
 	}
 
 	public static void print(Comparable[] a) {
@@ -35,7 +61,5 @@ public class MergeSort {
 		sort(a);
 
 		print(a);
-
 	}
-
 }
