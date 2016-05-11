427e60fd1c797bdb64b36ccfa5c468793650f404
Soonmok Kwon <ep1804@gmail.com>
2016-05-12 02:08:52 +0900

    drill05

--- drill/src/lp/Simplex.java
+++ drill/src/lp/Simplex.java
@@ -6,99 +6,115 @@ import java.util.Scanner;
 
 public class Simplex {
 
-	private int m, n;
-	private double[][] a;
-	private double max;
+	private int m;
+	private int n;
+	private double[][] S;
 
 	public double max() {
-		return max;
+		return -1 * S[m][n + m];
 	}
 
 	public Simplex(double[][] A, double[] b, double[] c) {
+
 		m = b.length;
 		n = c.length;
-		a = new double[m + 1][n + m + 1];
+		S = new double[m + 1][n + m + 1];
+
 		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++)
-				a[i][j] = A[i][j];
-			a[i][n + i] = 1;
-			a[i][n + m] = b[i];
+			for (int j = 0; j < n; j++) {
+				S[i][j] = A[i][j];
+			}
+			S[i][n + m] = b[i];
+			S[i][n + i] = 1;
 		}
 		for (int j = 0; j < n; j++)
-			a[m][j] = c[j];
+			S[m][j] = c[j];
 
-		while (true) {
-			int col = pivotCol();
-			if (col == -1)
-				break;
-			int row = pivotRow(col);
-			if (row != -1)
-				pivot(row, col);
+		System.out.println("tableau:");
+		printArr(S);
+
+		int pCol, pRow;
+		for (pCol = pivotCol(); pCol >= 0; pCol = pivotCol()) {
+			pRow = pivotRow(pCol);
+			pivot(pRow, pCol);
+
+			System.out.println("After pivoting with (" + pRow + ", " + pCol + "), tableau:");
+			printArr(S);
 		}
 
-		max = -a[m][n + m];
+		System.out.println("max:" + max());
 	}
 
 	private int pivotCol() {
-		for (int j = 0; j < m + n; j++)
-			if (a[m][j] > 0)
-				return j;
+		for (int i = 0; i < n + m; i++) {
+			if (S[m][i] > 0)
+				return i;
+		}
 		return -1;
 	}
 
 	private int pivotRow(int col) {
-		int row = -1;
-		double ratioMin = Double.POSITIVE_INFINITY;
-		for (int i = 0; i < m; i++) {
-			if (a[i][col] <= 0)
-				continue;
-			double ratio = a[i][m + n] / a[i][col];
-			if (ratioMin > ratio) {
-				ratioMin = ratio;
-				row = i;
+		int r = 0;
+		double minRatio = S[0][n + m] / S[0][col];
+		for (int i = 1; i < m; i++) {
+			double ratio = S[i][n + m] / S[i][col];
+			if (ratio >= 0 && ratio < minRatio) {
+				minRatio = ratio;
+				r = i;
 			}
 		}
-		return row;
+		return r;
 	}
 
 	private void pivot(int row, int col) {
+		double scale = S[row][col];
+		for (int j = 0; j <= n + m; j++) {
+			S[row][j] = S[row][j] / scale;
+		}
+
 		for (int i = 0; i <= m; i++) {
-			if (i == row)
-				continue;
-			double ratio = a[i][col] / a[row][col];
-			for (int j = 0; j <= m + n; j++) {
-				if (j == col)
-					a[i][j] = 0;
-				else
-					a[i][j] -= ratio * a[row][j];
+			if (i != row) {
+				double scale2 = S[i][col];
+				for (int j = 0; j <= n + m; j++)
+					S[i][j] -= S[row][j] * scale2;
+
 			}
 		}
-		double pv = a[row][col];
-		for (int j = 0; j <= m + n; j++)
-			a[row][j] /= pv;
+
+	}
+
+	private void printArr(double[][] x) {
+		int m = x.length;
+		int n = x[0].length;
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(x[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner in = new Scanner(new File("input/lp/brewer"));
 
-		int M = in.nextInt();
-		int N = in.nextInt();
-		double[][] A = new double[M][N];
-		double[] b = new double[M];
-		double[] c = new double[N];
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++)
-				A[i][j] = in.nextInt();
-			b[i] = in.nextInt();
+		Scanner in = new Scanner(new File("input/lp/brewer"));
+		int m = in.nextInt();
+		int n = in.nextInt();
+		double[][] A = new double[m][n];
+		double[] b = new double[m];
+		double[] c = new double[n];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				A[i][j] = in.nextDouble();
+			}
+			b[i] = in.nextDouble();
 		}
-		for (int j = 0; j < N; j++)
-			c[j] = in.nextInt();
+		for (int j = 0; j < n; j++)
+			c[j] = in.nextDouble();
+		in.close();
 
 		Simplex sx = new Simplex(A, b, c);
-		
-		System.out.println(sx.max());
 
-		in.close();
 	}
 
 }
