2c70917ef6f7ed31032feb74e9b3a2e6ee355f38
Soonmok Kwon <ep1804@gmail.com>
2016-10-25 05:27:35 +0900

    rpart wip

--- practice/hackerrank/functional_programming/src/rpart/Iris.scala
+++ practice/hackerrank/functional_programming/src/rpart/Iris.scala
@@ -4,43 +4,63 @@ import scala.collection.mutable.ListBuffer
 
 object Iris {
 
-  type Data = Array[(Array[Double], Boolean)]
-  type Rule = Array[Double] => Boolean
+  type X = Array[Double]
+  type Data = Array[(X, Boolean)]
+  type Rule = X => Boolean
 
-  class Node
+  class Node(xs: Array[X], rule: Rule) {
+    private val groups = xs.groupBy(rule)
+    val n0 = groups.getOrElse(false, Array()).size
+    val n1 = groups.getOrElse(true, Array()).size
+  }
+
+  class LeafNode(xs: Array[X], rule: Rule) extends Node(xs, rule) {
+    override def toString(): String = {
+      "LeafNode(" + n1 + "/" + (n0 + n1) + ")"
+    }
+  }
+
+  class StemNode(xs: Array[X], rule: Rule, left: Node, right: Node) extends Node(xs, rule) {
+    override def toString(): String = {
+      "StemNode(" + n1 + "/" + n0 + n1 + ", " + left + ", " + right + ")"
+    }
+  }
 
   def rpart(data: Data, rules: List[Rule], cp: Double): Node = {
-    new Node()
+    
+    //TODO
+    
+    new LeafNode(data.map(_._1), rules(0))
   }
 
   def main(args: Array[String]): Unit = {
     val file = System.getProperty("user.dir") + """\src\rpart\iris.csv"""
     val lines = io.Source.fromFile(file).getLines
     val vectors = lines.map(_.trim.split(",")).toArray
-    
+
     // Data: Array of (explanatory variables, response variable)
-    val data = vectors.map{case v => (v.take(4).map(_.toDouble), if(v(4) == "setosa") false else true)}.toArray
-    
+    val data = vectors.map { case v => (v.take(4).map(_.toDouble), if (v(4) == "setosa") false else true) }.toArray
+
     // Rules
-    val thresholds = data.map(_._1).transpose.map({x => 
+    val thresholds = data.map(_._1).transpose.map({ x =>
       val min = x.min
       val max = x.max
       min to max by (max - min) / 9
     })
     val rules = new ListBuffer[Rule]
-    for(i <- 0 until 4){
-      for(th <- thresholds(i)){
-        val ftn1 = (x: Array[Double]) => if(x(i) >= th) true else false
-        val ftn2 = (x: Array[Double]) => if(x(i) < th) true else false
+    for (i <- 0 until 4) {
+      for (th <- thresholds(i)) {
+        val ftn1 = (x: Array[Double]) => if (x(i) >= th) true else false
+        val ftn2 = (x: Array[Double]) => if (x(i) < th) true else false
         rules += ftn1
         rules += ftn2
-      }    
+      }
     }
-    
+
     println(data.toList)
     println(thresholds.toList)
     println(rules)
-    
+
     // Test rule functions
     println(rules(0)(Array(4.4, 0, 0, 0)))
     println(rules(0)(Array(4.2, 0, 0, 0)))
@@ -51,9 +71,8 @@ object Iris {
     println(rules(19)(Array(0, 2.1, 0, 0)))
     println(rules(19)(Array(0, 1.9, 0, 0)))
 
-    
     // Test rpart
-    
+
     print(rpart(data, rules.toList, 0.2))
   }
 }
