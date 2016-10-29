113babca0e131242ad71def3eca6fc40aa7d2f8f
Soonmok Kwon <ep1804@gmail.com>
2016-10-29 23:40:27 +0900

    quicks

--- drill/src/sort/QuickSorted.java
+++ drill/src/sort/QuickSorted.java
@@ -9,15 +9,41 @@ import java.util.Scanner;
 
 public class QuickSorted<T extends Comparable<T>> {
 
-	public T[] arr;
+	public T[] sorted;
 
 	public QuickSorted(T[] a) {
-		// TODO
+		sorted = Arrays.copyOf(a, a.length);
+		sort(sorted, 0, sorted.length);
 	}
 
-	// TODO private void sort
+	private void sort(T[] a, int lo, int hi) {
 
-	// TODO private void exch
+		if (hi - lo <= 1)
+			return;
+
+		T pivot = a[lo];
+		int lt = lo;
+		int gt = hi - 1;
+
+		for (int i = lt + 1; i <= gt; i++) {
+			int cmp = a[i].compareTo(pivot);
+
+			if (cmp > 0) {
+				exch(a, i--, gt--);
+			} else if (cmp < 0) {
+				exch(a, i, lt++);
+			}
+		}
+		
+		sort(a, lo, lt + 1);
+		sort(a, gt + 1, hi);
+	}
+
+	private void exch(T[] a, int x, int y) {
+		T tmp = a[x];
+		a[x] = a[y];
+		a[y] = tmp;
+	}
 
 	public static void print(Object[] a) {
 		StringBuilder sb = new StringBuilder();
@@ -36,7 +62,7 @@ public class QuickSorted<T extends Comparable<T>> {
 
 		QuickSorted<String> qs = new QuickSorted<String>(a);
 
-		print(qs.arr);
+		print(qs.sorted);
 
 	}
 
